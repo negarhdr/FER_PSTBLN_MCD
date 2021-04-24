1339720c4feda110475df71e0255deacf9bf48fe
Negar <negar.heidari@eng.au.dk>
2021-04-24 14:19:29 +0200

    shell scripts are updated

---
+++ run_casia.sh
@@ -0,0 +1,27 @@
+
+# CASIA-AGCN
+
+python main.py --config ./config/CASIA/AGCN/CASIA_0.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_1.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_2.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_3.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_4.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_5.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_6.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_7.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_8.yaml
+python main.py --config ./config/CASIA/AGCN/CASIA_9.yaml
+
+# CASIA-STBLN
+
+python main.py --config ./config/CASIA/STBLN/CASIA_0.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_1.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_2.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_3.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_4.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_5.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_6.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_7.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_8.yaml
+python main.py --config ./config/CASIA/STBLN/CASIA_9.yaml
+
---
+++ run_ck.sh
@@ -0,0 +1,28 @@
+
+# CK+-AGCN
+
+python main.py --config ./config/CK+/AGCN/CK1.yaml
+python main.py --config ./config/CK+/AGCN/CK2.yaml
+python main.py --config ./config/CK+/AGCN/CK3.yaml
+python main.py --config ./config/CK+/AGCN/CK4.yaml
+python main.py --config ./config/CK+/AGCN/CK5.yaml
+python main.py --config ./config/CK+/AGCN/CK6.yaml
+python main.py --config ./config/CK+/AGCN/CK7.yaml
+python main.py --config ./config/CK+/AGCN/CK8.yaml
+python main.py --config ./config/CK+/AGCN/CK9.yaml
+python main.py --config ./config/CK+/AGCN/CK10.yaml
+
+
+# CK+-STBLN
+
+python main.py --config ./config/CK+/AGCN/CK1.yaml
+python main.py --config ./config/CK+/AGCN/CK2.yaml
+python main.py --config ./config/CK+/AGCN/CK3.yaml
+python main.py --config ./config/CK+/AGCN/CK4.yaml
+python main.py --config ./config/CK+/AGCN/CK5.yaml
+python main.py --config ./config/CK+/AGCN/CK6.yaml
+python main.py --config ./config/CK+/AGCN/CK7.yaml
+python main.py --config ./config/CK+/AGCN/CK8.yaml
+python main.py --config ./config/CK+/AGCN/CK9.yaml
+python main.py --config ./config/CK+/AGCN/CK10.yaml
+
